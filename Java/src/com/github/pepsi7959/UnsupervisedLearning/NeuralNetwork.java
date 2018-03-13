a6832716a7b49c0f9857aca43c8308731a05452f
pepsi7959 <pepsi_kmutnb@windowslive.com>
2018-03-13 13:50:21 +0700

    add Layer class

--- Java/src/com/github/pepsi7959/UnsupervisedLearning/NeuralNetwork.java
+++ Java/src/com/github/pepsi7959/UnsupervisedLearning/NeuralNetwork.java
@@ -10,12 +10,22 @@ import com.github.pepsi7959.optimization.ActivationFunction;
 public class NeuralNetwork {
 	private LinkedList<Dataset> datasets;
 	private LinkedList<Double> ev;
+	private double nLayer = 1;
 	private Matrix weight;
 	private double step;
 	private double lr;
+	
 
 	public NeuralNetwork(LinkedList<Dataset> datasets, LinkedList<Double> ev, Matrix weight) {
 		this.datasets = datasets;
+		this.ev = ev;
+		this.weight = weight;
+		initialize();
+	}
+	
+	public NeuralNetwork(LinkedList<Input> inputs, LinkedList<Double> ev, Matrix weight, int nLayer) {
+		this.inputs = inputs;
+		this.nLayer = nLayer;
 		this.ev = ev;
 		this.weight = weight;
 		initialize();
@@ -48,10 +58,69 @@ public class NeuralNetwork {
 			Dataset in = (Dataset) dataset.next();
 			double costValue = costFunc(in, this.weight);
 			td.setData(row, 0, costValue);
-			double af = ActivationFunction.sinusoid(costValue);
-			System.out.println("input " + in.getData(1, 0) + " output : " + costValue + " activated : " + af);
+			double afValue = ActivationFunction.sinusoid(costValue);
+			System.out.println("input " + in.getData(0, 0)+", "+ in.getData(0, 1) + " output : " + costValue + " activated : " + afValue);
 			row++;
 		}
 		return td;
+	}
+	
+	public static Matrix train(LinkedList<Layer> layers) {
+		Iterator<Layer> iter_layer = layers.iterator();
+		Layer l = null;
+		int i = 0;
+		while( iter_layer.hasNext() ) {
+			l = iter_layer.next();
+			System.out.println("Layer "+i);
+			
+			i++;
+		}
+		return l.weight;
+	}
+	
+	static class Layer{
+		private LinkedList<Input> inputs;
+		private LinkedList<Input> outputs;
+		private Matrix weight;
+		private int dim;
+		private Layer next = null;
+		
+		public Layer(LinkedList<Input> inputs, Matrix weight, int dim) {
+			this.inputs = inputs;
+			this.weight = weight;
+			this.dim = dim;
+		}
+		
+		public Layer(int dim) {
+			this.dim = dim;
+		}
+		
+		
+		public void bind(Layer l) { this.next = l;}
+		
+		
+		public static LinkedList<Input> forwardProp(LinkedList<Input> inputs, Matrix Weight, int dim_outputs) {
+			LinkedList<Input> outputs = new LinkedList<Input>();
+			return outputs;
+		}
+		
+		public static void main(String[] args) {
+			LinkedList<Input> inputs = Input.fromFile("src\\UnsupervisedLearning\\inputs.csv");
+			Matrix weight = new Matrix(inputs.getFirst().getRow(), 1);
+			weight.random(0, 5);
+			
+
+			Layer l0 = new Layer(inputs, weight, 2);
+			Layer l1 = new Layer(2);
+			Layer l2 = new Layer(1);
+			
+			LinkedList<Layer> layers = new LinkedList<Layer>();
+			layers.addLast(l0);
+			layers.addLast(l1);
+			layers.addLast(l2);
+			
+			NeuralNetwork.train(layers);
+			
+		}
 	}
 }
